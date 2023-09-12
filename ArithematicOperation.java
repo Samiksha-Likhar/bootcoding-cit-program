7dbcf8128ca370a44ea7c3bd3e98413bc7082e01
Samiksha-Likhar <144773015+Samiksha-Likhar@users.noreply.github.com>
2023-09-12 16:04:42 +0530

    Add files via upload
    Java Codes

---
+++ ArithematicOperation.java
@@ -0,0 +1,18 @@
+public class ArithematicOperation
+{
+public static void main (String args[])
+{
+int a=20 ,b=48 ;
+int add,sub,mul,div;
+add = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+System.out.println("Addition :"+add);
+System.out.println("Substraction :"+sub);
+System.out.println("Multiplication :"+mul);
+System.out.println("Division :"+div);
+}
+}
+
+
